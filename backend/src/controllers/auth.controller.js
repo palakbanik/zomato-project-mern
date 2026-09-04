380fb36961df79eafbde1ffb0b60eb20aa618ef0
Palak Banik <polokbanik7@gmail.com>
2026-09-05 00:01:36 +0600

    create user and food-partner login, register, logout api done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 const userModel = require("../models/user.model");
+const foodPartnerModel = require("../models/foodpartner.model");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
@@ -104,8 +105,132 @@ async function loginUser(req, res) {
     }
 }
 
+// logout user by clearing the token
+function logoutUser(req, res) {
+    try {
+        res.clearCookie("token");
+
+        res.status(200).json({
+            message: "User logged out successfully",
+        });
+    } catch (err) {
+        res.status(500).json({
+            message: "Logout failed",
+        });
+    }
+}
+
+// register food partner
+async function registerFoodPartner(req, res) {
+    // destructure
+    const { name, email, password } = req.body;
+
+    // check if food partner is exists
+    const isAccountAlreadyExists = await foodPartnerModel.findOne({ email });
+    if (isAccountAlreadyExists) {
+        return res.status(400).json({
+            message: "Food partner account already exists.",
+        });
+    }
+
+    // hashed password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // create food partner account
+    const foodPartner = await foodPartnerModel.create({
+        name,
+        email,
+        password: hashedPassword,
+    });
+
+    // generate JWT token
+    const token = jwt.sign(
+        {
+            id: foodPartner._id,
+        },
+        process.env.JWT_SECRET,
+    );
+
+    // set token
+    res.cookie("token", token);
+
+    // send response
+    res.status(201).json({
+        message: "Food partner register successfully.",
+        foodPartner: {
+            _id: foodPartner._id,
+            name: foodPartner.name,
+            email: foodPartner.email,
+        },
+    });
+}
+
+async function loginFoodPartner(req, res) {
+    // destructure
+    const { email, password } = req.body;
+
+    // check food partner exists or not
+    const foodPartner = await foodPartnerModel.findOne({ email });
+    if (!foodPartner) {
+        return res.status(400).json({
+            message: "Invalid email or password",
+        });
+    }
+
+    // check password exists
+    const isPasswordValid = await bcrypt.compare(
+        password,
+        foodPartner.password,
+    );
+    if (!isPasswordValid) {
+        return res.status(400).json({
+            message: "Invalid email or password",
+        });
+    }
+
+    // generate JWT token
+    const token = jwt.sign(
+        {
+            id: foodPartner._id,
+        },
+        process.env.JWT_SECRET,
+    );
+
+    // set token
+    res.cookie("token", token);
+
+    // send response
+    res.status(200).json({
+        message: "Food partner logged in successfully.",
+        foodPartner: {
+            _id: foodPartner._id,
+            name: foodPartner.name,
+            email: foodPartner.email,
+        },
+    });
+}
+
+// logout food partner by clearing the token
+function logoutFoodPartner(req, res) {
+    try {
+        res.clearCookie("token");
+
+        res.status(200).json({
+            message: "Food Partner logged out successfully",
+        });
+    } catch (err) {
+        res.status(500).json({
+            message: "Logout failed",
+        });
+    }
+}
+
 // export multiple controllers
 module.exports = {
     registerUser,
     loginUser,
+    logoutUser,
+    registerFoodPartner,
+    loginFoodPartner,
+    logoutFoodPartner,
 };
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,9 +1,23 @@
 const express = require("express");
-const { registerUser, loginUser } = require("../controllers/auth.controller");
+const {
+    registerUser,
+    loginUser,
+    logoutUser,
+    registerFoodPartner,
+    loginFoodPartner,
+    logoutFoodPartner,
+} = require("../controllers/auth.controller");
 
 const router = express.Router();
 
+// user auth APIs
 router.post("/user/register", registerUser);
 router.post("/user/login", loginUser);
+router.get("/user/logout", logoutUser);
+
+// food partner auth APIs
+router.post("/food-partner/register", registerFoodPartner);
+router.post("/food-partner/login", loginFoodPartner);
+router.get("/food-partner/logout", logoutFoodPartner);
 
 module.exports = router;
